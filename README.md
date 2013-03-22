e584087326aa3906d8ed141b0f8db4fcff0828e2
Gabriel Bretschner <software@kanedo.net>
2013-03-22 13:16:03 +0100

    initial commit

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ Alfred2WriteLetter
 ==================
 
 Alfred2 Workflow to create a LaTeX-Letter-Template prefilled with the address selected
+
+To-Do
+-----
+* Since Alfred does not provide more than one address-detail, you need to insert the name of whom you want to write, by hand.  
+* Also there could be too much line-breaks in address field.
--- createLetter.php
+++ createLetter.php
@@ -1,8 +1,24 @@
 <?php
+function parseTemplate($address){
+	$adress_fields = explode("\n", $address);
+	$main_address = implode ("\\\\\n", $adress_fields);
+	
+	$file = file_get_contents("letter-template.tex");
+	$parsed = str_replace("%%ADDRESS%%", $main_address ,$file);
+	if(file_put_contents("letter-tmp.tex", $parsed) === false){
+		return false;
+	}
+	
+	return "letter-tmp.tex";
+}
 
-$address = "Altkötzschenbroda 40
-Radebeul
-01445
-Deutschland";
-
-?>
+function createLetter($address){
+	$file = parseTemplate($address);
+	if($file === false){
+		echo "Sorry, there's been a failure\n";
+		return;
+	}
+	shell_exec(("cat {$file} | /usr/local/bin/mate -at tex -m 'Letter.tex'"));
+	shell_exec("rm {$file}");
+	echo "Write Letter!";
+}
